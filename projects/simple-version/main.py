fc837c8c34ff53acd337f05494ac14e425b69ea1
Andreas Krammer <a-krammer@online.de>
2023-08-28 14:57:33 +0200

    Create API to process Excel data as dataframe, allow user to interact with GUI elements

--- projects/simple-version/main.py
+++ projects/simple-version/main.py
@@ -15,6 +15,10 @@ from fastapi.staticfiles import StaticFiles
 import threading
 import time
 
+ # GLOBAL VARIABLES
+# Loop through the items in the directory
+df_list = []
+
 fastapi_app = FastAPI()
 
 def run_fastapi():
@@ -29,6 +33,61 @@ async def read_root():
 
 # Mount the "static" folder to serve static files
 fastapi_app.mount("/static", StaticFiles(directory="static"), name="index")
+
+@fastapi_app.get("/process")
+async def process():
+    # Define the directory path
+    directory_path = "C://Users//user//Documents//inputs//"
+
+    # List all files and subdirectories in the specified directory
+    items = os.listdir(directory_path)
+
+    '''
+    INPUT READER
+    '''
+    for file_it in items:
+        file_path = os.path.join(directory_path, file_it)  # Full path of the item
+
+        # Check if the item is a file
+        if os.path.isfile(file_path):
+            with open(file_path, 'r') as file:
+                df = pd.read_excel(file_path, engine="openpyxl")
+                df_list.append(df) # add the newly read-in df to the list
+
+    # save Excel input naming
+    # Add your data processing logic here
+    processed_data = "Input data has been loaded. You can now decide what to do."
+    return {"message": processed_data}
+
+
+@fastapi_app.get("/execute_option")
+async def execute_option(selected_option: str):
+    # Define a switch-like dictionary to map options to functions
+    options_dict = {
+        "option1": function1,
+        "option2": function2,
+        "option3": function3
+    }
+
+    # Execute the corresponding function based on the selected option
+    if selected_option in options_dict:
+        selected_function = options_dict[selected_option]
+        result = selected_function()
+        return {"message": result}
+    else:
+        return {"message": "Option not found"}
+
+
+def function1():
+    return "Executing Function 1"
+
+
+def function2():
+    return "Executing Function 2"
+
+
+def function3():
+    return "Executing Function 3"
 
 def main():
     # Start FastAPI server in a separate thread
